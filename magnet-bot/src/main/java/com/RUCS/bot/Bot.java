5d678a15bd0445b78376835d4c72f5c7e97493cb
754liam <182143381+754liam@users.noreply.github.com>
2025-07-12 18:01:51 -0400

    Added bot logic, .gitignore, and config.properties files. Bot token is inside config.properties for security reasons.

--- magnet-bot/src/main/java/com/RUCS/bot/Bot.java
+++ magnet-bot/src/main/java/com/RUCS/bot/Bot.java
@@ -16,12 +16,42 @@ import net.dv8tion.jda.api.utils.MemberCachePolicy;
  * 7th line governs how jda fetches and caches member lists (might not be necessary for this bot though)
  * 8th line works with the 7th line and defines which server members jda should keep in cache
  */
-import okhttp3.internal.http2.Http2Connection.Listener;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import javax.security.auth.login.LoginException;
 // this class is just for exception purposes; if the bots login attempt fails this'll come in handy.
+//I dont need a lot of these imports, but they can be important for future reference. 
 
 public class Bot extends ListenerAdapter{ //using listeneradapter class imported on line 5 as the superclass; listeneradapter is relevant since its needed for listening. listeneradapter also implements eventlistener which provides implementations for all various discord events
+    public static void main(String[] args) throws LoginException, IOException{
+        Properties properties = new Properties();
+        try{ //error handling and importation of bot token located in a config properties file
+            FileInputStream fis = new FileInputStream("config.properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            System.out.println("Couldnt load config file");
+            e.printStackTrace();
+            return;
+        }
+        String token = properties.getProperty("BOT_TOKEN"); //hooking code up to bot and adding listening properties
+        JDABuilder.createDefault(token).addEventListeners(new Bot()).enableIntents(GatewayIntent.MESSAGE_CONTENT).build();
+        
+    }
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){ //code for configuring bot to respond to message when sent
+        if (event.getAuthor().isBot()){ //to not interfere with other bots. 
+            return;
+        }
+        else{
+         String messageContent = event.getMessage().getContentRaw(); //finds message content from chat. 
+         if (messageContent.equals("&ping")){
+            event.getChannel().sendMessage("pong🤺").queue(); //the bot is constantly listening for messages; queue is used to not freeze bot
+         }
+        }
 
+    }
 
 } 
